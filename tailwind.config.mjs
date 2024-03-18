35bde200eaccd840bcedbdaf6c7f42cec602527e
Katy Barboza <kbarbozab@unicartagena.edu.co>
2024-03-17 19:41:56 -0500

    Actualiza estilos de Introduce y Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,8 @@ export default {
 				'hover': 'rgba(232, 163, 236, 0.05)', //primary 5%
 				'dark': '#0a0a0a', //neutral 950
 				'dark-blur': 'rgba(10, 10, 10, 0.25)', 
-				'light': 'rgba(82, 82, 91, 0.05)', //neutral 600 5%
+				'light': '#52525B08', //neutral 600 5%
+				'gray': '#0D0D0D',
 				'white-dark': 'rgba(255, 255, 255, 0.40)',
 				'white-blur': 'rgba(255, 255, 255, 0.03)',
 				'yellow-light': '#FFD79C',
